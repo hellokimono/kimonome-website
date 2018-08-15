dd0ba699e5566a3f19e4f2ce13fc352ccdc267a4
groozin <tomasz.mikos@gmail.com>
2018-08-15 14:42:12 +0200

    Sets up webpack.

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./assets/js/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist/js"),
+    filename: "bundle.js"
+  },
+  optimization: {
+    minimize: false
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: "*.php", to: ".." },
+      { from: "style.css", to: ".." }
+    ])
+  ],
+  module: {
+    rules: [
+      {
+        test: /.scss$/,
+        use: [
+          //   {
+          //     loader: "file-loader",
+          //     options: {
+          //       name: "[name].css",
+          //       outputPath: "../css/"
+          //     }
+          //   },
+          { loader: "style-loader/url" },
+          {
+            loader: "css-loader"
+          },
+
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|otf)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "../fonts/"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(svg|jpg|png)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "../img/"
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
